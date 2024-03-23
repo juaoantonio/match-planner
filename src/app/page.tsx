750cc3583b448cc309cebc400f4e10e4c227b1f6
João Antônio Barbosa <joaobarbosadev@gmail.com>
2024-03-22 22:20:06 -0300

    feat: create default page for new user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return;
+import { TypographyP } from "@/components/typography/typography-p";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Frown } from "lucide-react";
+
+export default function MatchesListingPage() {
+  return (
+    <main className="grid h-[90svh] place-items-center">
+      <div className="space-y-8">
+        <div className="mx-4 flex flex-col items-center gap-2 text-muted-foreground">
+          <Frown className="" size={36} />
+          <TypographyP className="mt-0 text-center">
+            Parece que voce ainda nao esta em nenhum time.
+          </TypographyP>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <Button>Criar um novo time</Button>
+          <div className="flex w-2/4 items-center justify-center gap-2">
+            <Separator className="flex-1" />
+            <span className="text-xs">ou</span>
+            <Separator className="flex-1" />
+          </div>
+          <Button variant={"secondary"}>Entrar em um time existente</Button>
+        </div>
+      </div>
+    </main>
+  );
 }
